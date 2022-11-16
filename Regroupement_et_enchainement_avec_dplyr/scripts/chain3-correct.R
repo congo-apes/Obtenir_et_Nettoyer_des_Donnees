26b3a25e33fd78a0db128fba638c1de654402c26
Philip McElmurray <philmcelmurray@gmail.com>
2022-11-16 05:06:01 -0500

    Travaillé sur les erreurs de traduction.

--- Regroupement_et_enchainement_avec_dplyr/scripts/chain3-correct.R
+++ Regroupement_et_enchainement_avec_dplyr/scripts/chain3-correct.R
@@ -1,5 +1,5 @@
 # Utilisez filter() pour sélectionner toutes les lignes pour 
-# lesquelles taille_mo est inférieur ou égal à (<=) 0,5.
+# lesquelles taille_mo est inférieur ou égal à (<=) 0.5.
 # 
 # Si vous voulez que vos résultats soient imprimés sur la 
 # console, ajoutez print à la fin de votre chaîne.
